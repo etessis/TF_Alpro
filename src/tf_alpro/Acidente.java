879b30b631e0be72485f2eff36709ad8c7709674
etessis <etessis@gmail.com>
2014-11-14 20:17:54 -0200

    Versao inicial

---
+++ src/tf_alpro/Acidente.java
@@ -0,0 +1,39 @@
+
+package programadeacidentes;
+
+import java.util.Date;
+//testes
+public class Acidente {
+
+    private String rua;
+    private String tipo;
+    private Date dataHora;
+    private String diaSemana;
+    private int feridos;
+    private int mortes;
+    private int mortesPos;
+    private int fatais;
+    private int auto;
+    private int taxi;
+    private int lotacao;
+    private int onibusUrb;
+    private int onibusInt;
+    private int caminhao;
+    private int moto;
+    private int carroca;
+    private int bicicleta;
+    private String tempo;
+    private String noiteDia;
+    private String regiao;
+    
+    
+    public Acidente(){
+    
+        
+    }
+        
+        
+       
+   
+    
+}
